654c6bd8c9fda2a2fb59d71e3a1b8856bc30b3af
Ilya Grebnov <ilya.grebnov@gmail.com>
2013-03-12 11:08:37 -0700

    Updating Azure Store client certificates.

--- AzureStoreRP.Data/Authorization/AzureStoreAuthorization.cs
+++ AzureStoreRP.Data/Authorization/AzureStoreAuthorization.cs
@@ -62,8 +62,14 @@ namespace AzureStoreRP.Data.Authorization
             if (ConfigurationDataProvider.AzureStoreRequestAuthorization)
             {
                 if (clientCertificate == null || (
+                    // GTERdfeExtensibilityClientProd.cer, will ‎expire on Friday, ‎May ‎17, 2013
                     !clientCertificate.Thumbprint.Equals("C318C397979CB8BE103AA11346E4D82878C8A2B1", StringComparison.OrdinalIgnoreCase) &&
-                    !clientCertificate.Thumbprint.Equals("A9BE06E8881339D7B69F8421F51304431BED0EE7", StringComparison.OrdinalIgnoreCase)
+
+                    // BaltimoreRdfeExtensibilityClientProd.cer, will ‎expire on ‎Saturday, ‎February ‎14, ‎2015
+                    !clientCertificate.Thumbprint.Equals("‎F2693F8487AB975A28C19610A672E59DDCF873F2", StringComparison.OrdinalIgnoreCase) &&
+
+                    // BaltimoreRdfeExtensibilityClientStage.cer, will ‎expire on ‎Saturday, ‎February ‎14, ‎2015
+                    !clientCertificate.Thumbprint.Equals("‎19D02B07DEC22C0998BB266A7DA5BA8B4D42A0A6", StringComparison.OrdinalIgnoreCase)
                 ))
                 {
                     Logger.ErrorFormat(
--- AzureStoreRP.Webservice/Startup/InstallCertificates.cmd
+++ AzureStoreRP.Webservice/Startup/InstallCertificates.cmd
@@ -1,4 +1,11 @@
 certutil -f -addstore root Startup\Certificates\GTECyberTrustGlobalRoot.cer
-certutil -f -addstore CA   Startup\Certificates\MicrosoftInternetAuthority.cer
-certutil -f -addstore CA   Startup\Certificates\MicrosoftSecureServerAuthority.cer
-certutil -f -addstore My   Startup\Certificates\AzureStoreClientCertificate.cer
+certutil -f -addstore CA   Startup\Certificates\GTEMicrosoftInternetAuthority.cer
+certutil -f -addstore CA   Startup\Certificates\GTEMicrosoftSecureServerAuthority.cer
+certutil -f -addstore My   Startup\Certificates\GTERdfeExtensibilityClientProd.cer
+
+certutil -f -addstore root Startup\Certificates\BaltimoreCyberTrustRoot.cer
+certutil -f -addstore CA   Startup\Certificates\BaltimoreMicrosoftInternetAuthority.cer
+certutil -f -addstore CA   Startup\Certificates\BaltimoreMSITMachineAuthCA2.cer
+certutil -f -addstore My   Startup\Certificates\BaltimoreRdfeExtensibilityClientStage.cer
+certutil -f -addstore My   Startup\Certificates\BaltimoreRdfeExtensibilityClientProd.cer
+
